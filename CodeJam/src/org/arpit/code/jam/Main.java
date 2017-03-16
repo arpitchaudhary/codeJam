687be8951df4957c7e744bd8905c9ad50dd33dc3
Arpit Chaudhary <arpitchaudhary52@gmail.com>
2017-03-16 23:22:47 +1030

    Added comments in Main class to make code more readable.

--- CodeJam/src/org/arpit/code/jam/Main.java
+++ CodeJam/src/org/arpit/code/jam/Main.java
@@ -8,10 +8,19 @@ import java.io.IOException;
 
 import org.arpit.code.jam.april2016.CoinJam;
 
+/**
+ * @author arpit
+ * This Class is the main class which reads from input files and invokes specific
+ * class for the problem being solved.
+ */
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		/*Will read input file from this directory
+		* and will create the output file in this directory.
+		**/
 		String workingFolder = "C:\\Users\\arpit\\Downloads\\";
+		//without .in extension
 		String input = "C-small-attempt0";
 
 		BufferedReader br = new BufferedReader(new FileReader(workingFolder+input + ".in"));
@@ -20,6 +29,7 @@ public class Main {
 		StringBuilder output = new StringBuilder();
 		for(int i =1; i<=testCases; i++){
 			System.out.println("=========================Test Case #"+i+"=========================");
+			//Change the name of the class according to the problem being solved.
 			output = output.append("case #"+i+": "+CoinJam.process(br));
 			if(i<testCases){
 				output.append("\n");
